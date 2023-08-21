4029e24e56b672147406ee82684763c793ca6fb6
Muhamad Hamza Younas <127296848+afrazahmad0021@users.noreply.github.com>
2023-08-22 04:08:28 +0500

    update

--- routes/feed.js
+++ routes/feed.js
@@ -7,14 +7,13 @@ const isAuth = require('../middleware/IsAuthenticated');
 
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
-
 router.get('/products', shopController.getProducts);
 router.post('/create-order', shopController.postOrder);
 router.get('/orders', shopController.getOrders);
 router.get('/users', shopController.getUsers);
 router.delete('/user/:id', shopController.deleteUser);
 router.delete('/order/:id', shopController.deleteOrder);
+router.delete('/product/:id', shopController.deleteProduct);
 router.get('/orders_by_id/:id', shopController.ordersById);
 
 module.exports = router;
